fa85730f9667e824fbfb17162e9c7339f15fd33c
Andrew McLane <andrew_ryan@hush.ai>
2019-06-09 12:18:18 +0200

    Initial Commit

---
+++ Earthbound Release/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Earthbound Release
+//
+//  Created by Andrew on 09.06.19.
+//  Copyright © 2019 Smalli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
